74adc92dc466838c2146e08e1f3cd3907e530f6c
tokiorush <tokiorush@users.noreply.github.com>
2022-06-01 21:47:27 +0900

    tutorial phase 3 finished: added image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { Layout } from "components/layouts";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage alt="yuriage" src="../images/view.jpg" />
     </Layout>
   );
 };
