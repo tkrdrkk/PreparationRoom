cd021b4406c3e57aa81dd5423e47a35d437ff9c7
tokiorush <tokiorush@users.noreply.github.com>
2022-06-02 21:52:01 +0900

    tutorial phase 5 finished: added mdxtransformer

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,26 +1,34 @@
 import React from "react";
 import { Layout } from "components/layouts";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 // FIXME 型は？
 const Blog = (props: { data: any }) => {
   const { data } = props;
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.ud}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export const query = graphql`
   query {
-    allFile(filter: { sourceInstanceName: { eq: "blogs" } }) {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        body
       }
     }
   }
